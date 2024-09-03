227b1afd0b4310a2dcc6d5b2d97d7acec66af278
Sofaaaaaaaaa <sofat3026@gmail.com>
2024-09-03 15:50:39 +0300

    Практика первая

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+alert('jhujijmi');
+
+let name = prompt('Введите имя', 'user');
+let name1 = prompt('Введите фамилию', 'фамилия');
+let name2 = prompt('Введите отчество', 'отчество');
+
+alert(`Ваше имя ${name}`);
+alert(`Ваша фамилия ${name1}`);
+alert(`Ваше отчество ${name2} `);
+
+let isTrue = confirm('Это действительно ваше ФИО');
+alert (isTrue)
